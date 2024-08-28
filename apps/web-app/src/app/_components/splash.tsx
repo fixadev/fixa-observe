2db1fe7455abe3934d4d69e8220f402ffb296868
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-08-27 23:37:23 -0700

    animated placeholder

--- apps/web-app/src/app/_components/splash.tsx
+++ apps/web-app/src/app/_components/splash.tsx
@@ -1,33 +1,38 @@
-
-'use client'
-import React, { useState } from 'react';
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { SignUpDialog } from './dialogue';
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
+"use client";
+import React, { useEffect, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { SignUpDialog } from "./dialogue";
+import AnimatedPlaceholder from "./AnimatedPlaceholder";
 
 const ConceptInputPage = () => {
+  const [showPlaceholder, setShowPlaceholder] = useState(true);
+  const placeholders = [
+    "Visualize matrix multiplication step-by-step with 2x2 matrices",
+    "Illustrate the Doppler effect with sound waves",
+    "Demonstrate the water cycle, from evaporation to precipitation",
+    "Show the formation and lifecycle of a star in space",
+  ];
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-4">
-        <div className="w-1/2 flex flex-col items-center justify-center -mt-3 gap-6">
-        <h1 className="text-5xl font-md mb-8">Bring any concept to life</h1>
-            <div className="w-full flex flex-row gap-2">
-            <Input 
-            type="text" 
-            placeholder="Explain how matrices work" 
-            className="w-full text-xl bg-gray-800 text-white border-none rounded-lg py-7 px-6"
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-900 p-4 text-white">
+      <div className="-mt-3 flex w-1/2 flex-col items-center justify-center gap-6">
+        <h1 className="font-md mb-8 text-5xl">Bring any concept to life</h1>
+        <div className="flex w-full flex-row gap-2">
+          <div className="relative flex-grow">
+            {showPlaceholder && (
+              <AnimatedPlaceholder placeholders={placeholders} />
+            )}
+            <Input
+              type="text"
+              className="w-full rounded-lg border-none bg-gray-800 px-6 py-7 text-xl text-white"
+              onFocus={() => setShowPlaceholder(false)}
+              onBlur={() => setShowPlaceholder(true)}
             />
-            <SignUpDialog />
-            </div>
+          </div>
+          <SignUpDialog />
         </div>
-        
+      </div>
     </div>
   );
 };
