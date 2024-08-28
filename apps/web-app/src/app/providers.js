95df7373362d6b257b2f233c1cc36892b5248863
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-08-28 02:03:32 -0700

    fix error

--- apps/web-app/src/app/providers.js
+++ apps/web-app/src/app/providers.js
@@ -4,11 +4,12 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
     api_host: "/ingest",
     ui_host: "https://us.posthog.com",
   });
 }
+// @ts-ignore
 export function CSPostHogProvider({ children }) {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
