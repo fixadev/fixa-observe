3dabeb3dada47e572356deced8a92bf097523cd4
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-09-06 23:09:28 -0700

    finished bookcalldialog

--- apps/web-app/src/app/_components/BookCallDialog.tsx
+++ apps/web-app/src/app/_components/BookCallDialog.tsx
@@ -1,3 +1,5 @@
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
 import { Button } from "~/components/ui/button";
 import {
   Dialog,
@@ -16,21 +18,76 @@ export default function BookCallDialog({
   open: boolean;
   onOpenChange: (open: boolean) => void;
 }) {
+  const { user } = useUser();
+  const [state, setState] = useState<"initial" | "loading" | "success">(
+    "initial",
+  );
+  const [usageDescription, setUsageDescription] = useState<string>("");
+
+  const handleSubmit = () => {
+    setState("loading");
+    fetch(
+      "https://script.google.com/macros/s/AKfycbxJKx193ozKu98xA5u3geVwe5ewFzSDRPBYTpV3iHCSHILv3s2yJ_Gz1dcoZ9B1l-xe/exec",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          email:
+            user?.primaryEmailAddress?.emailAddress ??
+            "ERROR: NO EMAIL ADDRESS",
+          usageDescription,
+        }).toString(),
+      },
+    )
+      .then(() => {
+        setState("success");
+      })
+      .catch((e) => {
+        setState("initial");
+      });
+  };
+
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>you've run out of free generations!</DialogTitle>
-        </DialogHeader>
-        <DialogDescription>
-          tell us what you plan on using pixa for and we'll get back to you
-          within 24 hours.
-        </DialogDescription>
-        <Textarea placeholder="i plan on using pixa to..." />
-        <DialogFooter>
-          <Button>request access</Button>
-        </DialogFooter>
-      </DialogContent>
+      {state !== "success" && (
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>you've run out of free generations!</DialogTitle>
+          </DialogHeader>
+          <DialogDescription>
+            thank you for your interest in pixa! we're currently experiencing
+            high demand. tell us a bit about what you plan on using pixa for and
+            we'll get back to you within 24 hours.
+          </DialogDescription>
+          <Textarea
+            placeholder="i plan on using pixa to..."
+            value={usageDescription}
+            onChange={(e) => setUsageDescription(e.target.value)}
+          />
+          <DialogFooter>
+            <Button
+              disabled={state === "loading"}
+              type="submit"
+              onClick={handleSubmit}
+            >
+              {state === "loading" ? "requesting access..." : "request access"}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      )}
+      {state === "success" && (
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>request submitted!</DialogTitle>
+          </DialogHeader>
+          <DialogDescription>
+            check your email ({user?.primaryEmailAddress?.emailAddress}) for
+            further instructions.
+          </DialogDescription>
+        </DialogContent>
+      )}
     </Dialog>
   );
 }
