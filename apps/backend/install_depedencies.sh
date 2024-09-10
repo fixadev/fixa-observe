72d1af8271f1088052932733b50a922ac9550227
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2024-09-10 01:41:08 -0700

    removes unused files

--- apps/backend/install_depedencies.sh
+++ apps/backend/install_depedencies.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install -y libegl1-mesa libegl1-mesa-dev
 mkdir public
 
-conda create -n prod-env python=3.10
+conda create -n prod-env python=3.11
 conda activate prod-env
 conda install -c conda-forge manim
 pip install -r requirements.txt
