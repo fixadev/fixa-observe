2d180d905e584ccc9e5fbf15c7310142a263f5a6
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-09-10 01:01:22 -0700

    stream to ffmpeg directly

--- apps/backend/services/scenes.py
+++ apps/backend/services/scenes.py
@@ -1,3 +1,4 @@
+from memory_profiler import profile
 from queue import Queue
 from manim import *
 from manim.opengl import *
@@ -6,25 +7,35 @@ import math
 import numpy as np
 import time
 
-config.renderer = "opengl"
+# config.renderer = "opengl"
 config.write_to_movie = False
-# config.background_color = "#ffffff"
+config.disable_caching = True
 
 class BlankScene(Scene):
-    def __init__(self, frame_queue, commands, dimensions=(1920/4, 1080/4), frame_rate=60, start_time=time.time(), debug_mode=False, background_color='BLACK', *args, **kwargs):
+    def __init__(self, commands, ffmpeg_process, dimensions=(1920/4, 1080/4), frame_rate=60, start_time=time.time(), debug_mode=False, background_color='BLACK', *args, **kwargs):
         config.pixel_width = math.floor(dimensions[0])
         config.pixel_height = math.floor(dimensions[1])
         config.frame_rate = frame_rate
         config.progress_bar = "none"
         config.background_color = background_color
-        super().__init__(frame_queue, debug_mode=debug_mode, *args, **kwargs)
+        super().__init__(ffmpeg_process, debug_mode=debug_mode, *args, **kwargs)
 
-        assert isinstance(self.renderer, OpenGLRenderer), "This scene only works with the OpenGL renderer"
+        # assert isinstance(self.renderer, OpenGLRenderer), "This scene only works with the OpenGL renderer"
         self.commands = commands
         self.start_time = start_time
 
+    @profile
     def construct(self):
         self.interactive_embed(self.commands, self.start_time)
+
+        # title = Text("How Babies Are Made", color=BLUE).scale(1.2).shift(UP * 3)
+        # self.play(Write(title))
+        # sperm = Triangle(color=WHITE, fill_opacity=1).scale(0.2).shift(LEFT * 4)
+        # egg = Circle(color=PINK, fill_opacity=1).scale(0.5).shift(RIGHT * 4)
+        # self.play(FadeIn(sperm), FadeIn(egg))
+        # self.play(sperm.animate.move_to(egg.get_center()))
+        # fertilized_egg = Circle(color=YELLOW, fill_opacity=1).scale(0.6).move_to(egg.get_center())
+        # self.play(Transform(egg, fertilized_egg), FadeOut(sperm))
 
 class TestScene(Scene):
     def __init__(self, *args, **kwargs):
