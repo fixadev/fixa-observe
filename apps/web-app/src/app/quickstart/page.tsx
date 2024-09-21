1eec0d06dc701b7fc63803c435666671f0277ee2
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-09-21 15:49:27 -0700

    use react code blocks

--- apps/web-app/src/app/quickstart/page.tsx
+++ apps/web-app/src/app/quickstart/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+
 import PageHeader from "~/components/PageHeader";
+// import CodeBlock from "~/components/CodeBlock";
+import { CopyBlock, dracula } from "react-code-blocks";
+
+const code = `npm install @prisma/client`;
 
 export default function QuickstartPage() {
-  return <PageHeader title="quickstart" />;
+  return (
+    <div>
+      <PageHeader title="quickstart" />
+      <CopyBlock
+        text={code}
+        language="bash"
+        showLineNumbers={true}
+        wrapLongLines={true}
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        theme={dracula}
+      />
+    </div>
+  );
 }
