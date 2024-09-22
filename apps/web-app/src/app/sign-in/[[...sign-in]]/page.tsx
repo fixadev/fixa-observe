32332a13df1675e3cdf03dd2ab23da3848381b52
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-09-21 17:59:54 -0700

    small ui fixes

--- apps/web-app/src/app/sign-in/[[...sign-in]]/page.tsx
+++ apps/web-app/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-screen items-center justify-center bg-muted">
       <SignIn />
     </div>
   );
--- apps/web-app/src/components/CodeBlock.tsx
+++ apps/web-app/src/components/CodeBlock.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { ClipboardIcon, CheckIcon } from "@heroicons/react/24/outline";
+import { ClipboardIcon } from "@heroicons/react/24/outline";
 import {
   oneLight,
   type PrismLangauge,
   CodeBlock as ReactEmailCodeBlock,
 } from "@react-email/code-block";
 import { Button } from "./ui/button";
-import { useState } from "react";
 
 export default function CodeBlock({
   code,
@@ -16,16 +15,12 @@ export default function CodeBlock({
   code: string;
   language?: string;
 }) {
-  const [copied, setCopied] = useState(false);
-
   const handleCopy = async () => {
     await navigator.clipboard.writeText(code);
-    setCopied(true);
-    setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
   };
 
   return (
-    <div className="group relative">
+    <div className="group relative text-sm">
       <ReactEmailCodeBlock
         code={code}
         language={language as PrismLangauge}
@@ -38,11 +33,7 @@ export default function CodeBlock({
         className="absolute right-4 top-4 opacity-0 transition-opacity duration-300 group-hover:opacity-100"
         onClick={handleCopy}
       >
-        {copied ? (
-          <CheckIcon className="size-4 text-green-500" />
-        ) : (
-          <ClipboardIcon className="size-4" />
-        )}
+        <ClipboardIcon className="size-4" />
       </Button>
     </div>
   );
