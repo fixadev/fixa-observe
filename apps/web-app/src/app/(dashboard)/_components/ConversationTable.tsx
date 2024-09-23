640973bd39212b7b4136384c828cfe1616d5b1cd
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-09-22 17:40:16 -0700

    refresh button

--- apps/web-app/src/app/(dashboard)/_components/ConversationTable.tsx
+++ apps/web-app/src/app/(dashboard)/_components/ConversationTable.tsx
@@ -3,10 +3,13 @@
 import { DataTable } from "~/components/DataTable";
 import { columns } from "./ConversationTableColumns";
 import { api } from "~/trpc/react";
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { type PaginationState, type SortingState } from "@tanstack/react-table";
 import { useProject } from "~/app/contexts/projectContext";
 import { skipToken } from "@tanstack/react-query";
+import { Button } from "~/components/ui/button";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import { cn } from "~/lib/utils";
 
 export default function ConversationTable() {
   const initialSorting = useMemo(() => [{ id: "createdAt", desc: true }], []);
@@ -36,13 +39,16 @@ export default function ConversationTable() {
     return obj;
   }, [project]);
 
-  const { data: conversationsData, refetch: refetchConversations } =
-    api.conversations.getConversations.useQuery({
-      projectId: project?.id,
-      sorting: sorting,
-      pageSize: pagination.pageSize,
-      pageIndex: pagination.pageIndex,
-    });
+  const {
+    data: conversationsData,
+    refetch: refetchConversations,
+    isLoading: isLoadingConversations,
+  } = api.conversations.getConversations.useQuery({
+    projectId: project?.id,
+    sorting: sorting,
+    pageSize: pagination.pageSize,
+    pageIndex: pagination.pageIndex,
+  });
   const conversations = useMemo(() => {
     return (
       conversationsData?.conversations?.map((conversation) => ({
@@ -52,26 +58,64 @@ export default function ConversationTable() {
       })) ?? []
     );
   }, [outcomes, conversationsData]);
+  const [lastRefreshedAt, setLastRefreshedAt] = useState(new Date());
+  useEffect(() => {
+    if (conversationsData) {
+      setLastRefreshedAt(new Date());
+    }
+  }, [conversationsData]);
 
+  const [refreshing, setRefreshing] = useState(false);
+  const refresh = useCallback(() => {
+    setRefreshing(true);
+    void refetchConversations().finally(() => {
+      console.log("refreshed");
+      setRefreshing(false);
+    });
+  }, [refetchConversations]);
   useEffect(() => {
     const interval = setInterval(() => {
-      void refetchConversations();
+      if (!refreshing) {
+        refresh();
+      }
     }, 60 * 1000);
 
     return () => {
       clearInterval(interval);
     };
-  }, [refetchConversations]);
+  }, [refresh, refreshing]);
 
   return (
-    <DataTable
-      data={conversations}
-      columns={columns}
-      initialSorting={initialSorting}
-      initialPagination={initialPagination}
-      rowCount={conversationsData?.count}
-      onSortingChange={setSorting}
-      onPaginationChange={setPagination}
-    />
+    <div>
+      <div className="mb-2 flex items-center gap-2">
+        <Button
+          variant="outline"
+          onClick={refresh}
+          disabled={refreshing || isLoadingConversations}
+        >
+          <ArrowPathIcon
+            className={cn(
+              "mr-2 h-4 w-4",
+              (refreshing || isLoadingConversations) && "animate-spin",
+            )}
+          />{" "}
+          {refreshing || isLoadingConversations ? "Refreshing..." : "Refresh"}
+        </Button>
+        {!(refreshing || isLoadingConversations) && (
+          <span className="text-sm text-muted-foreground">
+            last refreshed at {lastRefreshedAt.toLocaleString()}
+          </span>
+        )}
+      </div>
+      <DataTable
+        data={conversations}
+        columns={columns}
+        initialSorting={initialSorting}
+        initialPagination={initialPagination}
+        rowCount={conversationsData?.count}
+        onSortingChange={setSorting}
+        onPaginationChange={setPagination}
+      />
+    </div>
   );
 }
