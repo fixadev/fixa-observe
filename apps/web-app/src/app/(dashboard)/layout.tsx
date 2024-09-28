532dd4e05f0886f1d268c0c0a3b6d54f6568dbd5
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-09-27 22:24:41 -0700

    removed useless crap

--- apps/web-app/src/app/(dashboard)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import NavWrapper from "./_components/NavWrapper";
-
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <NavWrapper>{children}</NavWrapper>;
-}
