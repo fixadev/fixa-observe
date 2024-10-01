b5ea49ce3c3762029b36b5a6f39df70f6da9dd8d
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-09-30 23:43:46 -0700

    add s3 credentials

--- apps/web-app/src/lib/building.ts
+++ apps/web-app/src/lib/building.ts
@@ -1,47 +1,44 @@
 import { z } from "zod";
-import { zfd } from 'zod-form-data';
+import { zfd } from "zod-form-data";
 import { type Building, type Space, type Attachment } from "@prisma/client";
 
 export const spaceSchema = z.object({
-    id: z.string(),
-    createdAt: z.date(),
-    updatedAt: z.date(),
-    buildingId: z.string(),
-    customProperties: z.record(z.string(), z.any()),
+  id: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  buildingId: z.string(),
+  customProperties: z.record(z.string(), z.any()),
 } satisfies { [K in keyof Space]: z.ZodType<Space[K]> });
-
 
 export type AttachmentSchema = z.infer<typeof attachmentSchema>;
 export const attachmentSchema = z.object({
-    id: z.string(),
-    createdAt: z.date(),
-    updatedAt: z.date(),
-    url: z.string(),
-    type: z.string(),
-    title: z.string(),
-    buildingId: z.string(),
+  id: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  url: z.string(),
+  type: z.string(),
+  title: z.string(),
+  buildingId: z.string(),
 } satisfies { [K in keyof Attachment]: z.ZodType<Attachment[K]> });
-
 
 export type AttributesSchema = z.infer<typeof attributesSchema>;
 export const attributesSchema = z.record(z.string(), z.string());
 export const buildingSchema = z.object({
-    id: z.string(),
-    createdAt: z.date(),
-    updatedAt: z.date(),
-    ownerId: z.string(),
-    address: z.string(),
-    photoUrls: z.array(z.string()),
-    attachmentIds: z.array(z.string()),
-    surveyIds: z.array(z.string()),
-    spaceIds: z.array(z.string()),
-    attributes: attributesSchema,
+  id: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  ownerId: z.string(),
+  address: z.string(),
+  photoUrls: z.array(z.string()),
+  attachmentIds: z.array(z.string()),
+  surveyIds: z.array(z.string()),
+  spaceIds: z.array(z.string()),
+  attributes: attributesSchema,
 } satisfies { [K in keyof Building]: z.ZodType<Building[K]> });
 
-
-export const photoUploadSchema = z.object({
-    buildingId: z.string(),
-    photos: z.array(zfd.file()),
+export const photoUploadSchema = zfd.formData({
+  buildingId: zfd.text(),
+  photos: z.array(zfd.file()),
 });
 
 export type CreateBuildingInput = z.infer<typeof createBuildingSchema>;
@@ -50,14 +47,13 @@ export const createBuildingSchema = buildingSchema.omit({ id: true });
 export type ImportBuildingsInput = z.infer<typeof importBuildingsInput>;
 export const importBuildingsInput = z.array(createBuildingSchema);
 
-
 export type HeaderMappingSchema = z.infer<typeof headerMappingSchema>;
 export const headerMappingSchema = z.record(
   z.string(),
   z.object({
     target: z.string(),
     isCustomProperty: z.boolean(),
-  })
+  }),
 );
 
 export type HeaderMapping = z.infer<typeof headerMappingSchema>;
--- apps/web-app/src/server/api/routers/building.ts
+++ apps/web-app/src/server/api/routers/building.ts
@@ -1,45 +1,79 @@
 import { z } from "zod";
+import { zfd } from "zod-form-data";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
-import { buildingSchema, importBuildingsInput, photoUploadSchema } from "~/lib/building";
-import { createOrUpdateBuildings, getBuildingDetails, updateBuildingDetails, addPhotoUrlsToBuilding, addAttachmentToBuilding, getAttributes } from "~/server/services/buildings";
+import {
+  buildingSchema,
+  importBuildingsInput,
+  photoUploadSchema,
+} from "~/lib/building";
+import {
+  createOrUpdateBuildings,
+  getBuildingDetails,
+  updateBuildingDetails,
+  addPhotoUrlsToBuilding,
+  addAttachmentToBuilding,
+  getAttributes,
+} from "~/server/services/buildings";
 import { uploadFileToS3 } from "~/server/utils/s3utils";
 
 export const buildingRouter = createTRPCRouter({
-
-    createOrUpdateBuildings: protectedProcedure.input(importBuildingsInput).mutation(async ({ ctx, input }) => {
-        return await createOrUpdateBuildings(input, ctx.userId, ctx.db);
+  createOrUpdateBuildings: protectedProcedure
+    .input(importBuildingsInput)
+    .mutation(async ({ ctx, input }) => {
+      return await createOrUpdateBuildings(input, ctx.userId, ctx.db);
     }),
 
-    getBuildingDetails: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ ctx, input }) => {
-        return await getBuildingDetails(input.id, ctx.userId, ctx.db);
+  getBuildingDetails: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      return await getBuildingDetails(input.id, ctx.userId, ctx.db);
     }),
 
-    updateBuildingDetails: protectedProcedure.input(buildingSchema).mutation(async ({ ctx, input }) => {
-        return await updateBuildingDetails(input, ctx.userId, ctx.db);
+  updateBuildingDetails: protectedProcedure
+    .input(buildingSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await updateBuildingDetails(input, ctx.userId, ctx.db);
     }),
 
-    addPhotosToBuilding: protectedProcedure.input(
-        photoUploadSchema
-    ).mutation(async ({ ctx, input }) => {
-        const photos = await Promise.all(input.photos.map(uploadFileToS3));
-        const photoUrls = photos.map(({ url }) => url);
-        return await addPhotoUrlsToBuilding(input.buildingId, photoUrls, ctx.userId, ctx.db);
+  addPhotosToBuilding: protectedProcedure
+    .input(photoUploadSchema)
+    .mutation(async ({ ctx, input }) => {
+      const photos = await Promise.all(input.photos.map(uploadFileToS3));
+      const photoUrls = photos.map(({ url }) => url);
+      return await addPhotoUrlsToBuilding(
+        input.buildingId,
+        photoUrls,
+        ctx.userId,
+        ctx.db,
+      );
     }),
 
-    addAttachmentToBuilding: protectedProcedure.input(
-        z.object({
-            buildingId: z.string(),
-            attachment: z.instanceof(File),
-            title: z.string(),
-            type: z.string(),
-        })
-    ).mutation(async ({ ctx, input }) => {
-        const attachment = await uploadFileToS3(input.attachment);
-        return await addAttachmentToBuilding(input.buildingId, attachment.url, attachment.type, input.title, ctx.userId, ctx.db);
+  addAttachmentToBuilding: protectedProcedure
+    .input(
+      z.object({
+        buildingId: z.string(),
+        attachmentUrl: z.string(),
+        title: z.string(),
+        type: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      //   console.log(
+      //     "####################### addAttachmentToBuilding!!!, input",
+      //     input,
+      //   );
+      //   const attachment = await uploadFileToS3(input.attachment);
+      return await addAttachmentToBuilding(
+        input.buildingId,
+        input.attachmentUrl,
+        input.type,
+        input.title,
+        ctx.userId,
+        ctx.db,
+      );
     }),
 
-    getAttributes: protectedProcedure.query(async ({ ctx }) => {
-        return await getAttributes(ctx.userId, ctx.db);
-    }),
+  getAttributes: protectedProcedure.query(async ({ ctx }) => {
+    return await getAttributes(ctx.userId, ctx.db);
+  }),
 });
-
