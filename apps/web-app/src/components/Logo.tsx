151181790f02410c0ea16bb6be9f777cb330b3b6
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2024-10-03 12:08:12 -0700

    changes logo

--- apps/web-app/src/components/Logo.tsx
+++ apps/web-app/src/components/Logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <Link href="/" className="text-xl font-medium sm:text-2xl">
-      pixa.dev
+      Titan
     </Link>
   );
 }
