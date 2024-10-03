0890816b6b047be8cd7d5d66870a44a2d452e4f1
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2024-10-03 12:54:41 -0700

    moves add attribute script to script folder

--- apps/web-app/src/server/api/routers/building.ts
+++ apps/web-app/src/server/api/routers/building.ts
@@ -59,29 +59,3 @@ export const buildingRouter = createTRPCRouter({
     return await buildingServiceInstance.getAttributes(ctx.userId);
   }),
 });
-
-
-
-
-async function uploadAttributes() {
-  const attributes = [
-    { label: "Address", type: "string" },
-    { label: "Size (SF)", type: "string" },
-    { label: "Divisibility (SF)", type: "string" },
-    { label: "NNN Asking Rate (SF/Mo)", type: "string" },
-    { label: "Opex (SF/Mo)", type: "string" },
-    { label: "Direct/Sublease", type: "string" },
-    { label: "Comments", type: "string" },
-  ];
-
-  for (const attribute of attributes) {
-    await db.attribute.create({
-      data: {
-        label: attribute.label,
-        type: attribute.type,
-        ownerId: null,
-      },
-    });
-  }
-}
-void uploadAttributes();
