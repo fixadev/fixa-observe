3ef0b5b683350e7aaf624e9be3908a56fd517d67
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-10-03 14:59:09 -0700

    renamed everything to properties/attributes

--- apps/web-app/src/app/(survey-dashboard)/projects/[projectId]/surveys/[surveyId]/_components/PropertiesTable.tsx
+++ apps/web-app/src/app/(survey-dashboard)/projects/[projectId]/surveys/[surveyId]/_components/PropertiesTable.tsx
@@ -41,143 +41,99 @@ import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
 import { PlusIcon, TrashIcon } from "@heroicons/react/24/solid";
 
-export type Space = {
+export type Property = {
   id: string;
-  name: string;
-  customProperties: Record<string, string>;
+  attributes: Record<string, string>;
   isNew?: boolean;
 };
-export type CustomProperty = {
+export type Attribute = {
   id: string;
-  name: string;
   label: string;
   isNew?: boolean;
 };
 
-const testSpaces: Space[] = [
+const attributes = {
+  floors: { id: "-1", label: "Floors" },
+  amenities: { id: "-2", label: "Amenities" },
+};
+const testAttributesOrder: Attribute[] = [
+  attributes.floors,
+  attributes.amenities,
+];
+
+const testProperties: Property[] = [
   {
     id: "1",
-    name: "Suite 101",
-    customProperties: {
-      floors: "3",
-      amenities: "Parking, Elevator, Conference Room",
+    attributes: {
+      [attributes.floors.id]: "3",
+      [attributes.amenities.id]: "Parking, Elevator, Conference Room",
     },
   },
   {
     id: "2",
-    name: "Suite 102",
-    customProperties: {
-      floors: "2",
-      amenities: "Kitchen, Gym, Rooftop Terrace",
+    attributes: {
+      [attributes.floors.id]: "2",
+      [attributes.amenities.id]: "Kitchen, Gym, Rooftop Terrace",
     },
   },
   {
     id: "3",
-    name: "Suite 103",
-    customProperties: {
-      floors: "1",
-      amenities: "Private Bathroom, Balcony, Storage Unit",
+    attributes: {
+      [attributes.floors.id]: "1",
+      [attributes.amenities.id]: "Private Bathroom, Balcony, Storage Unit",
     },
   },
   {
     id: "4",
-    name: "Suite 104",
-    customProperties: {
-      floors: "4",
-      amenities: "High-speed Internet, Open Floor Plan, Meeting Rooms",
+    attributes: {
+      [attributes.floors.id]: "4",
+      [attributes.amenities.id]:
+        "High-speed Internet, Open Floor Plan, Meeting Rooms",
     },
   },
   {
     id: "5",
-    name: "Suite 105",
-    customProperties: {
-      floors: "2",
-      amenities: "24/7 Security, Lounge Area, Bike Storage",
+    attributes: {
+      [attributes.floors.id]: "2",
+      [attributes.amenities.id]: "24/7 Security, Lounge Area, Bike Storage",
     },
   },
   {
     id: "6",
-    name: "Suite 106",
-    customProperties: {
-      floors: "3",
-      amenities: "Fitness Center, Cafeteria, Outdoor Workspace",
+    attributes: {
+      [attributes.floors.id]: "3",
+      [attributes.amenities.id]: "Fitness Center, Cafeteria, Outdoor Workspace",
     },
   },
   {
     id: "7",
-    name: "Suite 107",
-    customProperties: {
-      floors: "5",
-      amenities: "Executive Lounge, Helipad, Private Elevator",
+    attributes: {
+      [attributes.floors.id]: "5",
+      [attributes.amenities.id]: "Executive Lounge, Helipad, Private Elevator",
     },
   },
   {
     id: "8",
-    name: "Suite 108",
-    customProperties: {
-      floors: "1",
-      amenities: "Soundproof Rooms, Recording Studio, Green Screen Room",
+    attributes: {
+      [attributes.floors.id]: "1",
+      [attributes.amenities.id]:
+        "Soundproof Rooms, Recording Studio, Green Screen Room",
     },
   },
-  {
-    id: "9",
-    name: "Suite 109",
-    customProperties: {
-      floors: "2",
-      amenities: "Coworking Space, Nap Pods, Meditation Room",
-    },
-  },
-  {
-    id: "10",
-    name: "Suite 110",
-    customProperties: {
-      floors: "3",
-      amenities: "Indoor Garden, Aquarium, Art Gallery",
-    },
-  },
-  {
-    id: "11",
-    name: "Suite 111",
-    customProperties: {
-      floors: "4",
-      amenities: "Virtual Reality Lab, 3D Printing Room, Innovation Hub",
-    },
-  },
-  {
-    id: "12",
-    name: "Suite 112",
-    customProperties: {
-      floors: "2",
-      amenities:
-        "Rooftop Solar Panels, Electric Vehicle Charging, Recycling Center",
-    },
-  },
-  {
-    id: "13",
-    name: "Suite 113",
-    customProperties: {
-      floors: "1",
-      amenities: "Pet-friendly Office, Dog Park, Grooming Station",
-    },
-  },
-];
-const testCustomProperties: CustomProperty[] = [
-  { id: "-1", name: "floors", label: "Floors" },
-  { id: "-2", name: "amenities", label: "Amenities" },
 ];
 
 const DraggableHeader = ({
-  space,
-  renameSpace,
-  deleteSpace,
+  attribute,
+  renameAttribute,
+  deleteAttribute,
   draggingRow,
 }: {
-  space: Space;
-  renameSpace: (name: string) => void;
-  deleteSpace: () => void;
+  attribute: Attribute;
+  renameAttribute: (name: string) => void;
+  deleteAttribute: () => void;
   draggingRow: boolean;
 }) => {
-  const [isEditing, setIsEditing] = useState(space.isNew ?? false);
+  const [isEditing, setIsEditing] = useState(attribute.isNew ?? false);
 
   const {
     transform,
@@ -187,7 +143,7 @@ const DraggableHeader = ({
     attributes,
     listeners,
   } = useSortable({
-    id: !draggingRow ? space.id : "",
+    id: !draggingRow ? attribute.id : "",
   });
 
   const style: CSSProperties = {
@@ -209,7 +165,7 @@ const DraggableHeader = ({
       <div className="flex items-center justify-between gap-2">
         {isEditing ? (
           <Input
-            defaultValue={space.name}
+            defaultValue={attribute.label}
             onKeyDown={(e) => {
               if (e.key === "Enter") {
                 setIsEditing(false);
@@ -217,18 +173,18 @@ const DraggableHeader = ({
             }}
             onBlur={() => setIsEditing(false)}
             onChange={(e) => {
-              renameSpace(e.target.value);
+              renameAttribute(e.target.value);
             }}
             autoFocus
             onFocus={(e) => e.target.select()}
           />
         ) : (
           <Button variant="ghost" onClick={() => setIsEditing(true)}>
-            {space.name}
+            {attribute.label}
           </Button>
         )}
         <div className="invisible flex text-muted-foreground group-hover:visible">
-          <Button size="icon" variant="ghost" onClick={deleteSpace}>
+          <Button size="icon" variant="ghost" onClick={deleteAttribute}>
             <TrashIcon className="size-4" />
           </Button>
         </div>
@@ -245,28 +201,24 @@ const DraggableHeader = ({
 };
 
 const DraggableRow = ({
-  spaces,
   property,
-  renameProperty,
+  attributes,
   deleteProperty,
   draggingRow,
   setDraggingRow,
 }: {
-  spaces: Space[];
-  property: CustomProperty;
-  renameProperty: (label: string) => void;
-  deleteProperty: () => void;
+  property: Property;
+  attributes: Attribute[];
   draggingRow: boolean;
+  deleteProperty: (id: string) => void;
   setDraggingRow: (draggingRow: boolean) => void;
 }) => {
-  const [isEditing, setIsEditing] = useState(property.isNew ?? false);
-
   const {
     transform,
     transition,
     setNodeRef,
     isDragging,
-    attributes,
+    attributes: dragAttributes,
     listeners,
   } = useSortable({
     id: draggingRow ? property.id : "",
@@ -293,18 +245,30 @@ const DraggableRow = ({
       >
         <DragHandleDots2Icon
           className="size-4"
-          {...attributes}
+          {...dragAttributes}
           {...listeners}
         />
       </TableCell>
-      {spaces.map((space) => {
+      {attributes.map((attribute) => {
         return (
-          <DraggableCell key={space.id} id={space.id} draggingRow={draggingRow}>
-            <Input defaultValue={space.customProperties[property.name]} />
+          <DraggableCell
+            key={attribute.id}
+            id={attribute.id}
+            draggingRow={draggingRow}
+          >
+            <Input defaultValue={property.attributes[attribute.id]} />
           </DraggableCell>
         );
       })}
-      <TableCell></TableCell>
+      <TableCell>
+        <Button
+          size="icon"
+          variant="ghost"
+          onClick={() => deleteProperty(property.id)}
+        >
+          <TrashIcon className="size-4" />
+        </Button>
+      </TableCell>
     </TableRow>
   );
 };
@@ -338,15 +302,18 @@ const DraggableCell = ({
 };
 
 export default function PropertiesTable() {
-  const [spaces, setSpaces] = useState<Space[]>(testSpaces);
-  const [propertiesOrder, setPropertiesOrder] =
-    useState<CustomProperty[]>(testCustomProperties);
+  const [properties, setProperties] = useState<Property[]>(testProperties);
+  const [attributesOrder, setAttributesOrder] =
+    useState<Attribute[]>(testAttributesOrder);
   const [draggingRow, setDraggingRow] = useState<boolean>(false);
 
-  const colIds = useMemo(() => spaces.map((space) => space.id), [spaces]);
+  const colIds = useMemo(
+    () => properties.map((property) => property.id),
+    [properties],
+  );
   const rowIds = useMemo(
-    () => propertiesOrder.map((property) => property.id),
-    [propertiesOrder],
+    () => attributesOrder.map((attribute) => attribute.id),
+    [attributesOrder],
   );
 
   const handleDragEnd = useCallback(
@@ -354,13 +321,13 @@ export default function PropertiesTable() {
       const { active, over } = event;
       if (active && over && active.id !== over.id) {
         if (draggingRow) {
-          setPropertiesOrder((data) => {
+          setAttributesOrder((data) => {
             const oldIndex = rowIds.findIndex((id) => id === active.id);
             const newIndex = rowIds.findIndex((id) => id === over.id);
             return arrayMove(data, oldIndex, newIndex);
           });
         } else {
-          setSpaces((data) => {
+          setProperties((data) => {
             const oldIndex = colIds.findIndex((id) => id === active.id);
             const newIndex = colIds.findIndex((id) => id === over.id);
             return arrayMove(data, oldIndex, newIndex);
@@ -377,13 +344,12 @@ export default function PropertiesTable() {
     useSensor(KeyboardSensor, {}),
   );
 
-  const addSpace = useCallback(() => {
-    setSpaces((data) => [
+  const addProperty = useCallback(() => {
+    setProperties((data) => [
       ...data,
       {
         id: crypto.randomUUID(),
-        name: "New space",
-        customProperties: {},
+        attributes: {},
         isNew: true,
       },
     ]);
@@ -394,26 +360,16 @@ export default function PropertiesTable() {
     });
   }, []);
 
-  const renameSpace = useCallback((id: string, name: string) => {
-    setSpaces((data) => {
-      const index = data.findIndex((space) => space.id === id);
-      if (index === -1) return data;
-      const newData = [...data];
-      newData[index]!.name = name;
-      return newData;
-    });
-  }, []);
-
-  const addProperty = useCallback(() => {
-    setPropertiesOrder((data) => [
+  const addAttribute = useCallback(() => {
+    setAttributesOrder((data) => [
       ...data,
-      { id: crypto.randomUUID(), name: "new", label: "New field", isNew: true },
+      { id: crypto.randomUUID(), label: "New field", isNew: true },
     ]);
   }, []);
 
-  const renameProperty = useCallback((id: string, label: string) => {
-    setPropertiesOrder((data) => {
-      const index = data.findIndex((property) => property.id === id);
+  const renameAttribute = useCallback((id: string, label: string) => {
+    setAttributesOrder((data) => {
+      const index = data.findIndex((attribute) => attribute.id === id);
       if (index === -1) return data;
       const newData = [...data];
       newData[index]!.label = label;
@@ -421,9 +377,9 @@ export default function PropertiesTable() {
     });
   }, []);
 
-  const deleteSpace = useCallback((id: string) => {
-    setSpaces((data) => {
-      const index = data.findIndex((space) => space.id === id);
+  const deleteProperty = useCallback((id: string) => {
+    setProperties((data) => {
+      const index = data.findIndex((property) => property.id === id);
       if (index === -1) return data;
       const newData = [...data];
       newData.splice(index, 1);
@@ -431,9 +387,9 @@ export default function PropertiesTable() {
     });
   }, []);
 
-  const deleteProperty = useCallback((id: string) => {
-    setPropertiesOrder((data) => {
-      const index = data.findIndex((property) => property.id === id);
+  const deleteAttribute = useCallback((id: string) => {
+    setAttributesOrder((data) => {
+      const index = data.findIndex((attribute) => attribute.id === id);
       if (index === -1) return data;
       const newData = [...data];
       newData.splice(index, 1);
@@ -465,18 +421,20 @@ export default function PropertiesTable() {
                     : horizontalListSortingStrategy
                 }
               >
-                {spaces.map((space) => (
+                {attributesOrder.map((attribute) => (
                   <DraggableHeader
-                    key={space.id}
-                    space={space}
-                    renameSpace={(name) => renameSpace(space.id, name)}
-                    deleteSpace={() => deleteSpace(space.id)}
+                    key={attribute.id}
+                    attribute={attribute}
+                    renameAttribute={(name) =>
+                      renameAttribute(attribute.id, name)
+                    }
+                    deleteAttribute={() => deleteAttribute(attribute.id)}
                     draggingRow={draggingRow}
                   ></DraggableHeader>
                 ))}
               </SortableContext>
               <TableCell className="justify-center-background sticky right-0 z-20 flex bg-background">
-                <Button size="icon" variant="ghost" onClick={addSpace}>
+                <Button size="icon" variant="ghost" onClick={addAttribute}>
                   <PlusIcon className="size-4" />
                 </Button>
               </TableCell>
@@ -491,15 +449,12 @@ export default function PropertiesTable() {
                   : horizontalListSortingStrategy
               }
             >
-              {propertiesOrder.map((property) => {
+              {properties.map((property) => {
                 return (
                   <DraggableRow
                     key={property.id}
-                    spaces={spaces}
                     property={property}
-                    renameProperty={(label) =>
-                      renameProperty(property.id, label)
-                    }
+                    attributes={attributesOrder}
                     deleteProperty={() => deleteProperty(property.id)}
                     draggingRow={draggingRow}
                     setDraggingRow={setDraggingRow}
