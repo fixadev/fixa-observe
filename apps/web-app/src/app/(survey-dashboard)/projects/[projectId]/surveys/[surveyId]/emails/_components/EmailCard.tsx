45183ede157fb59115a3fd2caaf55ec07fd5aa79
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-10-03 20:58:06 -0700

    email dialog

--- apps/web-app/src/app/(survey-dashboard)/projects/[projectId]/surveys/[surveyId]/emails/_components/EmailCard.tsx
+++ apps/web-app/src/app/(survey-dashboard)/projects/[projectId]/surveys/[surveyId]/emails/_components/EmailCard.tsx
@@ -1,14 +1,10 @@
 "use client";
 
 import { formatDistanceToNow } from "date-fns";
+import { useState } from "react";
+import { AutosizeTextarea } from "~/components/ui/autosize-textarea";
 import { Avatar, AvatarImage, AvatarFallback } from "~/components/ui/avatar";
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-} from "~/components/ui/card";
+import { Button } from "~/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -18,9 +14,26 @@ import {
 } from "~/components/ui/dialog";
 import { cn } from "~/lib/utils";
 
+export type Email = {
+  id: number;
+  content: string;
+  createdAt: Date;
+  sender: {
+    name: string;
+    photoUrl: string;
+    email: string;
+  };
+};
+
 const testEmail = {
+  id: 1,
   createdAt: new Date(),
-  content: `Dear Mark,
+  sender: {
+    name: "Mark",
+    photoUrl: "https://github.com/shadcn.png",
+    email: "mark@example.com",
+  },
+  content: `Dear Colin,
 
 I hope this email finds you well. I'm reaching out regarding the property at 123 Main St. that I recently came across in my search for potential investments. I'm very interested in this property and would appreciate if you could provide me with some additional information:
 
@@ -35,64 +48,70 @@ Additionally, I was wondering if it would be possible to schedule a viewing of t
 Thank you for your time and assistance. I look forward to hearing back from you soon.
 
 Best regards,
-Colin`,
+Mark`,
 };
 
 export default function EmailCard({
+  email,
   unread = false,
   expanded = false,
-  expandable = false,
-  onExpand,
+  onClick,
+  className,
 }: {
+  email: Email;
   unread?: boolean;
   expanded?: boolean;
   expandable?: boolean;
-  onExpand?: (expanded: boolean) => void;
+  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+  className?: string;
 }) {
   return (
     <div
-      className="cursor-pointer overflow-hidden rounded-md border border-input p-4 shadow-sm hover:bg-muted"
-      onClick={() => {
-        if (expandable) {
-          onExpand?.(!expanded);
-        }
-      }}
+      className={cn(
+        "overflow-hidden rounded-md border border-input shadow-sm",
+        className,
+      )}
     >
-      <div className="overflow-hidden">
-        <div className="flex items-center gap-2">
-          {!expanded && (
-            <div
-              className={cn([
-                "-ml-2 size-2 shrink-0 rounded-full bg-blue-500",
-                unread ? "visible" : "invisible",
-              ])}
-            ></div>
-          )}
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <div className="space-y-1 overflow-hidden">
-            <div className="flex items-center justify-between">
-              <div className="text-base font-medium">You</div>
-              <div className="text-xs text-muted-foreground">
-                {formatDistanceToNow(new Date(testEmail.createdAt), {
-                  addSuffix: true,
-                })}
-              </div>
+      <div
+        className="flex cursor-pointer items-center gap-2 rounded-md p-4 hover:bg-muted"
+        onClick={(e) => {
+          onClick?.(e);
+        }}
+      >
+        <div
+          className={cn([
+            "-ml-2 size-2 shrink-0 rounded-full bg-blue-500",
+            unread ? "visible" : "invisible",
+          ])}
+        ></div>
+        <Avatar>
+          <AvatarImage src={email.sender.photoUrl} />
+          <AvatarFallback>
+            {email.sender.name
+              .split(" ")
+              .map((name) => name[0])
+              .join("")}
+          </AvatarFallback>
+        </Avatar>
+        <div className="flex-1 space-y-1 overflow-hidden">
+          <div className="flex items-center justify-between">
+            <div className="text-base font-medium">{email.sender.name}</div>
+            <div className="text-xs text-muted-foreground">
+              {formatDistanceToNow(new Date(email.createdAt), {
+                addSuffix: true,
+              })}
             </div>
-            {!expanded && (
-              <div className="overflow-hidden truncate text-sm text-muted-foreground">
-                {testEmail.content}
-              </div>
-            )}
           </div>
-          <div className="flex-1" />
+          {!expanded && (
+            <div className="overflow-hidden truncate text-sm text-muted-foreground">
+              {email.content}
+            </div>
+          )}
         </div>
       </div>
       {expanded && (
-        <div>
-          {testEmail.content.split("\n").map((line, index) => (
+        <div className="p-4 pl-6">
+          {email.content.split("\n").map((line, index) => (
             <p key={index} className="min-h-5 text-sm">
               {line}
             </p>
@@ -106,19 +125,52 @@ export default function EmailCard({
 export function EmailCardWithDialog({ unread = false }: { unread?: boolean }) {
   return (
     <EmailDialog>
-      <EmailCard unread={unread} />
+      <EmailCard email={testEmail} unread={unread} />
     </EmailDialog>
   );
 }
 
 export function EmailDialog({ children }: { children: React.ReactNode }) {
+  const [emails, setEmails] = useState<(Email & { expanded: boolean })[]>([
+    { ...testEmail, id: 1, expanded: false },
+    { ...testEmail, id: 2, expanded: false },
+    { ...testEmail, id: 3, expanded: true },
+  ]);
+
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent>
+      <DialogContent className="max-w-4xl">
         <DialogHeader>
           <DialogTitle>Email thread</DialogTitle>
         </DialogHeader>
+        <div className="-mx-2 flex h-[80vh] flex-col gap-2 overflow-y-auto px-2">
+          {emails.map((email) => (
+            <EmailCard
+              key={email.id}
+              className="shrink-0"
+              email={email}
+              expanded={email.expanded}
+              onClick={() => {
+                setEmails(
+                  emails.map((e) =>
+                    e.id === email.id ? { ...e, expanded: !e.expanded } : e,
+                  ),
+                );
+              }}
+            />
+          ))}
+          <div className="my-2">
+            <AutosizeTextarea
+              minHeight={100}
+              className="mb-2"
+              placeholder="Write a reply..."
+            />
+            <div className="flex gap-2">
+              <Button>Send</Button>
+            </div>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
