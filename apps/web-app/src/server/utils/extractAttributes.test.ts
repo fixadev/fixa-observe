7ecba836d07dff2615c884521fe8ffc2495cc173
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-10-12 11:51:27 -0700

    made extract attributes dynamic

--- apps/web-app/src/server/utils/extractAttributes.test.ts
+++ apps/web-app/src/server/utils/extractAttributes.test.ts
@@ -1,15 +1,18 @@
 import { extractAttributes } from "./extractAttributes";
 
 async function test() {
-  const result = await extractAttributes(`
+  const result = await extractAttributes(
+    `
   Hi,
 
   The asking rate is $5.50 per square foot, and the operating expenses are $0.50 per square foot.
-  The property is available on November 10, 2024.
+  And yes, the property is available
 
   Best,
   Jonathan
-`);
+`,
+    ["askingRate", "opEx"],
+  );
 
   console.log(result);
 }
