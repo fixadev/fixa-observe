97957e634d7e253b6104cc574801a966d6a1d476
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-10-16 15:18:36 -0700

    fixed some type errors

--- apps/web-app/src/app/(survey-dashboard)/projects/[projectId]/surveys/[surveyId]/brochures/[brochureId]/editor/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { PDFEditor } from "./_components/PDFEditor";
-
-export default function Page({ params }: { params: { brochureId: string } }) {
-  return <PDFEditor brochureId={params.brochureId} />;
-}
