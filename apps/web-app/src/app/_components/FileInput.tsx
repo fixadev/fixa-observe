034579788642082c94a36eb9a3aa29ac47589514
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-10-27 19:21:33 -0700

    fixed file input

--- apps/web-app/src/app/_components/FileInput.tsx
+++ apps/web-app/src/app/_components/FileInput.tsx
@@ -1,39 +1,46 @@
-import { useRef, type ReactNode } from "react";
+import { useRef, type ReactNode, forwardRef } from "react";
 
-export const FileInput = ({
-  children,
-  accept = "application/pdf",
-  handleFilesChange,
-}: {
+interface FileInputProps {
   children: ReactNode;
   accept?: string;
   handleFilesChange: (files: FileList) => void;
-}) => {
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const handleButtonClick = () => {
-    fileInputRef.current?.click();
-  };
+}
 
-  const onFilesChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    if (files) {
-      handleFilesChange(files);
-    }
-  };
+export const FileInput = forwardRef<HTMLDivElement, FileInputProps>(
+  ({ children, accept = "application/pdf", handleFilesChange }, ref) => {
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  return (
-    <>
-      <div onClick={handleButtonClick} className="inline cursor-pointer">
-        {children}
-      </div>
-      <input
-        ref={fileInputRef}
-        type="file"
-        id="pdfFileSelector"
-        className="hidden"
-        accept={accept}
-        onChange={onFilesChangeHandler}
-      />
-    </>
-  );
-};
+    const handleButtonClick = () => {
+      fileInputRef.current?.click();
+    };
+
+    const onFilesChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const files = e.target.files;
+      if (files) {
+        handleFilesChange(files);
+      }
+    };
+
+    return (
+      <>
+        <div
+          ref={ref}
+          onClick={handleButtonClick}
+          className="inline cursor-pointer"
+        >
+          {children}
+        </div>
+        <input
+          ref={fileInputRef}
+          type="file"
+          id="pdfFileSelector"
+          className="hidden"
+          accept={accept}
+          onChange={onFilesChangeHandler}
+        />
+      </>
+    );
+  },
+);
+
+FileInput.displayName = "FileInput";
