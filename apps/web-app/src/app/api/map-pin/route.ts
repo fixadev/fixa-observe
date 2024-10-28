23338984b0d1957ddee73c84985b6c2b2dcd05d3
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2024-10-28 00:01:36 -0700

    use system font

--- apps/web-app/src/app/api/map-pin/route.ts
+++ apps/web-app/src/app/api/map-pin/route.ts
@@ -29,7 +29,7 @@ export async function GET(request: Request) {
         dy=".3em" 
         fill="white" 
         font-size="${fontSize}"
-        font-family="sans-serif"
+        font-family="system-ui"
       >${number}</text>
     </svg>
   `);
