840925263b531aaa73fa17e5d811eb4891f46969
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2024-11-05 04:06:31 -0800

    adds retell API key

--- apps/web-app/src/server/api/routers/agent.ts
+++ apps/web-app/src/server/api/routers/agent.ts
@@ -1,27 +1,38 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { agentService } from "~/server/services/agent";
+import { env } from "~/env";
 
 const agentServiceInstance = agentService();
+
+const retellApiKey = env.RETELL_API_KEY;
 
 export const agentRouter = createTRPCRouter({
   listAgents: protectedProcedure
     .input(
       z.object({
-        apiKey: z.string(),
+        apiKey: z.string().optional(),
       }),
     )
     .mutation(async ({ input }) => {
-      return await agentServiceInstance.listAgents(input.apiKey);
+      return await agentServiceInstance.listAgents(
+        input.apiKey ?? retellApiKey,
+      );
     }),
   getFlow: protectedProcedure
-    .input(z.object({ apiKey: z.string(), agentId: z.string() }))
+    .input(z.object({ apiKey: z.string().optional(), agentId: z.string() }))
     .mutation(async ({ input }) => {
-      return await agentServiceInstance.getFlow(input.apiKey, input.agentId);
+      return await agentServiceInstance.getFlow(
+        input.apiKey ?? retellApiKey,
+        input.agentId,
+      );
     }),
   listCalls: protectedProcedure
-    .input(z.object({ apiKey: z.string(), agentId: z.string() }))
+    .input(z.object({ apiKey: z.string().optional(), agentId: z.string() }))
     .mutation(async ({ input }) => {
-      return await agentServiceInstance.listCalls(input.apiKey, input.agentId);
+      return await agentServiceInstance.listCalls(
+        input.apiKey ?? retellApiKey,
+        input.agentId,
+      );
     }),
 });
