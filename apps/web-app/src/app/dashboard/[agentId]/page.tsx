f1a9aacf1ddc3720759ed8f7a57c58371274fa91
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-11-09 22:06:49 -0800

    begin agents page

--- apps/web-app/src/app/dashboard/[agentId]/page.tsx
+++ apps/web-app/src/app/dashboard/[agentId]/page.tsx
@@ -1,3 +1,55 @@
+import { RocketLaunchIcon } from "@heroicons/react/24/solid";
+import { useMemo } from "react";
+import {
+  Accordion,
+  AccordionTrigger,
+  AccordionContent,
+  AccordionItem,
+} from "~/components/ui/accordion";
+import { Button } from "~/components/ui/button";
+import { TEST_AGENT } from "~/lib/test-data";
+
 export default function AgentPage({ params }: { params: { agentId: string } }) {
-  return <div className="container">AgentPage {params.agentId}</div>;
+  const agent = useMemo(() => {
+    return TEST_AGENT;
+  }, []);
+
+  return (
+    <div className="container">
+      {/* header */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <div className="flex flex-col gap-1">
+            <div className="text-2xl font-medium">{agent.name}</div>
+            <div className="text-sm text-muted-foreground">
+              {agent.phoneNumber}
+            </div>
+          </div>
+          <Button variant="outline">edit</Button>
+        </div>
+        <Button size="lg" className="flex items-center gap-2">
+          run test <RocketLaunchIcon className="size-4" />
+        </Button>
+      </div>
+
+      {/* content */}
+      <div className="mt-8">
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="hover:no-underline">
+              <div className="flex items-center gap-2">
+                test agents{" "}
+                <div className="rounded-full bg-muted px-2 py-1 text-xs font-normal">
+                  10
+                </div>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the same design as the existing system.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </div>
+  );
 }
