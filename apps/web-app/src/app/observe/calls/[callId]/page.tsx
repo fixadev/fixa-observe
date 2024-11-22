049170b2c4d32480a86ceb575f4a4e2ea5c8af3b
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-11-21 16:02:07 -0800

    space to play/pause

--- apps/web-app/src/app/observe/calls/[callId]/page.tsx
+++ apps/web-app/src/app/observe/calls/[callId]/page.tsx
@@ -1,15 +1,31 @@
 "use client";
 
 import CallDetails from "~/components/dashboard/CallDetails";
-import { AudioProvider } from "~/components/hooks/useAudio";
+import { AudioProvider, useAudio } from "~/components/hooks/useAudio";
 import { TEST_OBSERVE_CALLS } from "~/lib/test-data";
 import { api } from "~/trpc/react";
 
-export default function CallPage({ params }: { params: { callId: string } }) {
-  const { data: call } = api._call.getCall.useQuery(params.callId);
-  console.log(call);
+export function CallPage({ params }: { params: { callId: string } }) {
+  // const { data: call } = api._call.getCall.useQuery(params.callId);
+  // console.log(call);
+
+  const { play, pause, isPlaying } = useAudio();
   return (
-    <AudioProvider>
+    <div
+      className="h-screen w-screen"
+      autoFocus
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === " ") {
+          e.preventDefault();
+          if (isPlaying) {
+            pause();
+          } else {
+            play();
+          }
+        }
+      }}
+    >
       <CallDetails
         call={TEST_OBSERVE_CALLS[0]!}
         botName="jordan"
@@ -18,6 +34,18 @@ export default function CallPage({ params }: { params: { callId: string } }) {
         avatarUrl="/images/agent-avatars/jordan.png"
         type="latency"
       />
+    </div>
+  );
+}
+
+export default function CallPageWrapper({
+  params,
+}: {
+  params: { callId: string };
+}) {
+  return (
+    <AudioProvider>
+      <CallPage params={params} />
     </AudioProvider>
   );
 }
