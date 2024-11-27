e6eef1ac530a311ba298705c025b883a7da29830
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2024-11-26 22:27:14 -0500

    handles double submissions

--- apps/node-server/src/services/uploadCallToDB.ts
+++ apps/node-server/src/services/uploadCallToDB.ts
@@ -6,15 +6,20 @@ export const uploadCallToDB = async (
   audioUrl: string,
   agentId?: string,
   regionId?: string,
+  createdAt?: Date,
 ) => {
   const { url } = await uploadFromPresignedUrl(callId, audioUrl);
-  return await db.callRecording.create({
-    data: {
+  return await db.callRecording.upsert({
+    where: {
+      id: callId,
+    },
+    create: {
       id: callId,
       audioUrl: url,
-      createdAt: new Date(),
+      createdAt: createdAt || new Date(),
       agentId,
       regionId,
     },
+    update: {},
   });
 };
