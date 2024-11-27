baaad752bf47dedddd33369a464cc42b26a29d06
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2024-11-26 20:48:14 -0500

    fixes port

--- apps/transcription-service/src/main.py
+++ apps/transcription-service/src/main.py
@@ -35,4 +35,4 @@ async def transcribe(request: TranscribeRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
