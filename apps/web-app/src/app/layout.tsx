8a7f0bd837c42e308bfc46ecfe6f4ae8a35431fa
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2024-12-09 00:56:25 -0800

    redeploy

--- apps/web-app/src/app/layout.tsx
+++ apps/web-app/src/app/layout.tsx
@@ -8,7 +8,6 @@ import { CSPostHogProvider } from "./providers";
 import { ClerkProvider } from "@clerk/nextjs";
 
 // import { dark } from "@clerk/themes";
-
 import ChildrenWrapper from "./_components/ChildrenWrapper";
 import { TooltipProvider } from "~/components/ui/tooltip";
 export const metadata: Metadata = {
