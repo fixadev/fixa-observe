a3fcf95d51c48dbf38a7fb847858bb3c45bd3499
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2024-12-11 04:09:01 -0800

    adds command to copy prisma

--- apps/node-server/esbuild.js
+++ apps/node-server/esbuild.js
@@ -1,19 +1,13 @@
 const { build } = require('esbuild-wasm');
 
-const { copy } = require('esbuild-plugin-copy');
-
 build({
   entryPoints: ['src/index.ts'],
   bundle: true,
   platform: 'node',
   format: 'cjs',
+  sourcemap: true,
   outfile: 'dist/index.js',
-  plugins: [
-    copy({
-      assets: {
-        from: ['../../node_modules/.pnpm/@prisma+client@*/node_modules/.prisma/client/libquery_engine-*'],
-        to: ['./'],
-      },
-    }),
-  ],
-}).catch(() => process.exit(1));
+}).catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
