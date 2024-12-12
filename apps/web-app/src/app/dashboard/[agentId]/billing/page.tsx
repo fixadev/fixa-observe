c22fed9bc0ae2cfa512166a21245b8df8df20d52
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-12-11 19:19:12 -0800

    free tests logic

--- apps/web-app/src/app/dashboard/[agentId]/billing/page.tsx
+++ apps/web-app/src/app/dashboard/[agentId]/billing/page.tsx
@@ -1,12 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/solid";
+import { NUM_FREE_TESTS } from "@repo/types/src/constants";
 import Link from "next/link";
+import { useMemo } from "react";
 import { SidebarTrigger } from "~/components/ui/sidebar";
+import { cn } from "~/lib/utils";
 
 export default function BillingPage({
   params,
 }: {
   params: { agentId: string };
 }) {
+  const hasPaymentMethod = useMemo(() => {
+    return true;
+  }, []);
+
+  const freeTestsLeft = useMemo(() => {
+    return 1;
+  }, []);
+
   return (
     <div className="h-full">
       <div className="sticky top-0 z-20 flex h-14 w-full items-center justify-between border-b border-input bg-sidebar px-4 lg:h-[60px]">
@@ -21,46 +35,82 @@ export default function BillingPage({
         {/* Plan Section */}
         <section>
           <div className="mb-4 text-lg font-medium">current plan</div>
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-2 text-sm">
-              <div className="w-24 font-medium">plan</div>
-              <div>pay as you go</div>
-            </div>
-            <div className="flex items-center gap-2 text-sm">
-              <div className="w-24 font-medium">amount</div>
-              <div>$0.20 / minute</div>
-            </div>
-          </div>
-          <div className="mt-2 flex items-center gap-2 text-sm text-gray-500">
-            <div>need enterprise volume discounts?</div>
-            <Button variant="link" asChild>
-              <Link
-                href="https://cal.com/team/fixa/fixa-enterprise"
-                target="_blank"
-              >
-                talk to us
-              </Link>
-            </Button>
-          </div>
+          {hasPaymentMethod ? (
+            <>
+              <div className="flex flex-col gap-2">
+                <div className="flex items-center gap-2 text-sm">
+                  <div className="w-24 font-medium">plan</div>
+                  <div>pay as you go</div>
+                </div>
+                <div className="flex items-center gap-2 text-sm">
+                  <div className="w-24 font-medium">amount</div>
+                  <div>$0.20 / minute</div>
+                </div>
+              </div>
+              <div className="mt-2 flex items-center gap-2 text-sm text-gray-500">
+                <div>need enterprise volume discounts?</div>
+                <Button variant="link" asChild>
+                  <Link
+                    href="https://cal.com/team/fixa/fixa-enterprise"
+                    target="_blank"
+                  >
+                    talk to us
+                  </Link>
+                </Button>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex flex-col gap-2">
+                <div className="flex items-center gap-2 text-sm">
+                  <div className="w-24 font-medium">plan</div>
+                  <div>free</div>
+                </div>
+                <div className="flex items-center gap-2 text-sm">
+                  <div className="w-24 font-medium">amount</div>
+                  <div
+                    className={cn(
+                      freeTestsLeft <= 0 && "text-red-500",
+                      "text-sm",
+                    )}
+                  >
+                    {freeTestsLeft} free tests left
+                  </div>
+                </div>
+                {freeTestsLeft <= 0 && (
+                  <div className="flex w-[16.5rem] items-center gap-2 rounded-md border border-yellow-600 bg-yellow-50 p-1 text-sm font-medium text-muted-foreground text-yellow-600">
+                    <ExclamationTriangleIcon className="h-4 w-4 text-yellow-600" />
+                    upgrade now to continue using fixa.
+                  </div>
+                )}
+              </div>
+              <Button className="mt-2 w-[16.5rem]">upgrade</Button>
+              <div className="mt-1 w-[16.5rem] text-xs text-muted-foreground">
+                usage based pricing. only pay for what you use.
+              </div>
+            </>
+          )}
         </section>
 
         {/* Billing Details Section */}
-        <section>
-          <div className="mb-4 text-lg font-medium">billing details</div>
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-2 text-sm">
-              <div className="w-24 font-medium">name</div>
-              <div>jonathan liu</div>
+        {hasPaymentMethod && (
+          <section>
+            <div className="mb-4 text-lg font-medium">billing details</div>
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2 text-sm">
+                <div className="w-24 font-medium">name</div>
+                <div>jonathan liu</div>
+              </div>
+              <div className="flex items-center gap-2 text-sm">
+                <div className="w-24 font-medium">email</div>
+                <div>jonathan@fixa.dev</div>
+              </div>
             </div>
-            <div className="flex items-center gap-2 text-sm">
-              <div className="w-24 font-medium">email</div>
-              <div>jonathan@fixa.dev</div>
-            </div>
-          </div>
-          <Button variant="outline" className="mt-4">
-            edit info
-          </Button>
-        </section>
+            <Button variant="outline" className="mt-4">
+              edit info
+            </Button>
+          </section>
+        )}
 
         {/* Usage Section */}
         <section>
--- packages/types/src/constants.ts
+++ packages/types/src/constants.ts
@@ -1,3 +1,5 @@
 export const ERROR_LABELS = {
   transcription: "transcription error",
 };
+
+export const NUM_FREE_TESTS = 3;
