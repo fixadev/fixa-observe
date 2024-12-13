580ca5b448bc1294b7c0c5e4d010bc6c45aff145
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-12-13 05:43:03 -0800

    conflicting pages

--- apps/web-app/src/app/dashboard/(agents)/page.tsx
+++ apps/web-app/src/app/dashboard/(agents)/page.tsx
@@ -1,31 +1,11 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { PlusIcon } from "@heroicons/react/24/solid";
-import { Button } from "~/components/ui/button";
-import AgentCard from "~/components/dashboard/AgentCard";
-import { AddAgentModal } from "./_components/AddAgentModal";
-import { api } from "~/trpc/react";
+export default function RedirectPage() {
+  const redirectUrl = "/dashboard/new";
 
-export default function DashboardPage() {
-  const { data: agents, refetch: refetchAgents } = api.agent.getAll.useQuery();
+  if (redirectUrl) {
+    redirect(redirectUrl);
+  }
 
-  return (
-    <div className="flex flex-1 flex-col gap-8 overflow-hidden">
-      <div className="container flex items-center justify-between">
-        <div className="text-2xl font-medium">agents.</div>
-        <div>
-          <AddAgentModal refetchAgents={refetchAgents}>
-            <Button className="flex items-center gap-2">
-              <PlusIcon className="size-4" /> create agent
-            </Button>
-          </AddAgentModal>
-        </div>
-      </div>
-      <div className="flex-1 overflow-y-auto pb-8">
-        <div className="container grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {agents?.map((agent) => <AgentCard key={agent.id} agent={agent} />)}
-        </div>
-      </div>
-    </div>
-  );
+  return null;
 }
