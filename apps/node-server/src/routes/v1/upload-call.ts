5fb119d9c4d66bd20bd6e0f85e0e0c9572942f88
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-12-16 16:43:54 -0800

    deduct free calls

--- apps/node-server/src/routes/v1/upload-call.ts
+++ apps/node-server/src/routes/v1/upload-call.ts
@@ -1,6 +1,9 @@
 import { Request, Response, Router } from "express";
 import { validateUploadCallParams } from "../../middlewares/validateParams";
 import { addCallToQueue } from "../../services/aws";
+import userServiceClient from "../../clients/userServiceClient";
+import { PublicMetadata } from "@repo/types/src";
+import { env } from "../../env";
 
 const uploadCallRouter = Router();
 
@@ -20,6 +23,34 @@ uploadCallRouter.post(
       } = req.body;
 
       if (regionId) metadata.regionId = regionId;
+
+      // Determine whether to decrement free calls left
+      try {
+        const user = await userServiceClient.getUser(res.locals.userId);
+        if (user?.publicMetadata) {
+          const metadata = user.publicMetadata as PublicMetadata;
+          if (
+            metadata.stripeCustomerId ||
+            user.id === env.NEXT_PUBLIC_11X_USER_ID
+          ) {
+            // User is paid user!
+          } else {
+            // User is free user! Decrement free calls left
+            if (
+              metadata.freeObservabilityCallsLeft &&
+              metadata.freeObservabilityCallsLeft > 0
+            ) {
+              await userServiceClient.decrementFreeObservabilityCallsLeft(
+                res.locals.userId,
+              );
+            } else {
+              return res
+                .status(403)
+                .json({ success: false, error: "no free calls left" });
+            }
+          }
+        }
+      } catch (error) {}
 
       console.log(
         "ADDING CALL TO QUEUE",
