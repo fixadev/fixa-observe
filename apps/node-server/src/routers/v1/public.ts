81af27d347c9dcb0b30b437fdabfa669b97f176b
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2024-12-26 01:49:35 -0500

    adds alert router

--- apps/node-server/src/routers/v1/public.ts
+++ apps/node-server/src/routers/v1/public.ts
@@ -4,6 +4,8 @@ import testsRouter from "./routes/tests";
 import personaRouter from "./routes/persona";
 import agentRouter from "./routes/agent";
 import scenarioRouter from "./routes/scenario";
+import alertRouter from "./routes/alert";
+
 const v1Router = express.Router();
 
 v1Router.use("/upload-call", uploadCallRouter);
@@ -11,4 +13,5 @@ v1Router.use("/tests", testsRouter);
 v1Router.use("/persona", personaRouter);
 v1Router.use("/agent", agentRouter);
 v1Router.use("/scenario", scenarioRouter);
+v1Router.use("/alert", alertRouter);
 export default v1Router;
