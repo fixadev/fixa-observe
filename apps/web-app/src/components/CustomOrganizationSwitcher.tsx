945fba47d467c6eeccf09a5227ca285b813e05ac
Jonathan Liu <liu.z.jonathan@gmail.com>
2025-01-04 14:51:42 -0800

    orgSwitcher redirect to correct place

--- apps/web-app/src/components/CustomOrganizationSwitcher.tsx
+++ apps/web-app/src/components/CustomOrganizationSwitcher.tsx
@@ -4,7 +4,14 @@ import { BillingPage } from "~/components/BillingPage";
 
 export function CustomOrganizationSwitcher() {
   return (
-    <OrganizationSwitcher hidePersonal hideSlug>
+    <OrganizationSwitcher
+      hidePersonal
+      hideSlug
+      afterCreateOrganizationUrl={window.location.href}
+      afterLeaveOrganizationUrl={
+        "/org-selection?redirectUrl=" + window.location.href
+      }
+    >
       <OrganizationSwitcher.OrganizationProfilePage
         label="billing"
         labelIcon={<CreditCardIcon />}
