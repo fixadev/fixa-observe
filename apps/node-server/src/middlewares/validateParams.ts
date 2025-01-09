ecdd1704f8e19201cdabdb612f2520530b14ffbb
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2025-01-09 12:36:34 -0800

    push fix

--- apps/node-server/src/middlewares/validateParams.ts
+++ apps/node-server/src/middlewares/validateParams.ts
@@ -18,21 +18,20 @@ export const validateUploadCallParams = async (
       error: `Missing required fields: ${missingFields.join(", ")}`,
     });
   }
-
-  try {
-    const numberOfChannels = await getNumberOfAudioChannels(stereoRecordingUrl);
-    if (numberOfChannels !== 2) {
-      return res.status(400).json({
-        success: false,
-        error: "Audio file must be stereo (2 channels)",
-      });
-    }
-  } catch (error) {
-    return res.status(400).json({
-      success: false,
-      error:
-        "Could not verify audio file channel metadata. Please ensure the URL points to a valid stereo audio file and is accessible.",
-    });
-  }
+  // try {
+  //   const numberOfChannels = await getNumberOfAudioChannels(stereoRecordingUrl);
+  //   if (numberOfChannels !== 2) {
+  //     return res.status(400).json({
+  //       success: false,
+  //       error: "Audio file must be stereo (2 channels)",
+  //     });
+  //   }
+  // } catch (error) {
+  //   return res.status(400).json({
+  //     success: false,
+  //     error:
+  //       "Could not verify audio file channel metadata. Please ensure the URL points to a valid stereo audio file and is accessible.",
+  //   });
+  // }
   next();
 };
