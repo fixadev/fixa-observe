f79276addef84db1236efcc447148c7731a45096
Jonathan Liu <liu.z.jonathan@gmail.com>
2025-01-14 18:13:18 -0800

    onboard page

--- apps/web-app/src/app/observe/layout.tsx
+++ apps/web-app/src/app/observe/layout.tsx
@@ -1,53 +1,12 @@
-"use client";
-
 import { ObserveStateProvider } from "~/components/hooks/useObserveState";
 import ObserveSidebar from "~/components/observe/ObserveSidebar";
 import { SidebarProvider } from "~/components/ui/sidebar";
-import { useOrganization } from "@clerk/nextjs";
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { type PublicMetadata } from "@repo/types/src";
-import Spinner from "~/components/Spinner";
 
 export default function ObserveLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { organization, isLoaded } = useOrganization();
-  const [isOnboarded, setIsOnboarded] = useState(false);
-
-  const checkIsOnboarded = useCallback(() => {
-    console.log(
-      "======================================== Checking if onboarded! =============================================",
-    );
-    if (!organization) return;
-    const metadata = organization.publicMetadata as PublicMetadata;
-    if (metadata.onboarded) {
-      setIsOnboarded(true);
-    } else {
-      setTimeout(() => {
-        void organization?.reload().then(() => {
-          checkIsOnboarded();
-        });
-      }, 1000);
-    }
-  }, [organization]);
-
-  useEffect(() => {
-    if (isLoaded) {
-      checkIsOnboarded();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isLoaded]);
-
-  if (!isOnboarded) {
-    return (
-      <div className="flex size-full items-center justify-center">
-        <Spinner />
-      </div>
-    );
-  }
-
   return (
     <ObserveStateProvider>
       <SidebarProvider>
