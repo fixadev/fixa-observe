51e7eeb6b8b716cf8c692ca3374c9e65e2408b57
Jonathan Liu <liu.z.jonathan@gmail.com>
2025-01-14 18:20:26 -0800

    better calls exist logic

--- apps/web-app/src/app/observe/saved/[searchId]/_components/SavedSearchPage.tsx
+++ apps/web-app/src/app/observe/saved/[searchId]/_components/SavedSearchPage.tsx
@@ -128,7 +128,15 @@ export default function SavedSearchPage({
     );
   }, [calls, selectedCallId, callOverrides]);
 
-  if (!callsExist && !isLoadingCallsExist) {
+  if (isLoadingCallsExist) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (!callsExist) {
     return <NoCallsCard />;
   }
 
