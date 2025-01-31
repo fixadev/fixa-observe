09f42cb009772c6bad0894408a4bb9bcd86a3c3d
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2025-01-30 17:00:19 -0800

    redeploy

--- apps/node-server/src/workers/queueConsumer.ts
+++ apps/node-server/src/workers/queueConsumer.ts
@@ -115,6 +115,7 @@ async function runQueueConsumer() {
     await new Promise((resolve) => setTimeout(resolve, 1000));
   }
 }
+// comment
 export async function startQueueConsumer() {
   while (true) {
     try {
